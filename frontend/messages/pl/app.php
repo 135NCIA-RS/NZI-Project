3daa432dffdecc7cbe8ffbd3ee265b8e972c4088
marta-alexandra <martagiecewicz@wp.pl>
2015-11-30 15:23:45 +0100

    translacje z angielskiego na polski
    translacje z angielskiego na polski

--- frontend/messages/pl/app.php
+++ frontend/messages/pl/app.php
@@ -1,8 +1,26 @@
 <?php
 
 return [
-    'Congratulations!' => 'Gratulacje!',
-    'Language' => 'Język',
+    'Congratulations!'  => 'Gratulacje!',
+    'Language'          => 'Język',
+    'Toogle navigation' => 'Przelacz nawigacje',
+    'Sign in' => 'Zaloguj',
+    "You've not signed in" => 'Nie jestes zalogowany',
+    'Register'=> 'Zarejestruj sie',
+    'LogIn'=> 'Zaloguj sie'
+    'Main Navigation' =>'Pasek nawigacji'
+    'Home' =>'Strona glowna'
+    'Sign in to start your session' =>
+    'I forgot my password' => 'Zapomnialem hasla'
+    'Request password reset' => 'zadanie resetowania hasla'
+    'Please choose your new password'=> 'Prosze wpisac nowe haslo'
+    'Username'=>'Nazwa uzytkownika'
+    'Password'=>'Haslo'
+    'Retype Password'=>'Powtorz haslo'
+    'Register a new membership'=>'Utworz konto'
+    'I already have a membership'=>'Jestem już uzytkownikiem'
+    
+    
 ];
 
 
