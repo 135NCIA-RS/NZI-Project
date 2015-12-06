df93024ffe5d9e0e2cb67e11aea40d3f51e74aa6
Greg Lieske <grzesiu.lieske@gmail.com>
2015-12-07 00:12:41 +0100

    InvalidDateException and Extended Features for UserService  TODO setting data

--- frontend/components/UserService.php
+++ frontend/components/UserService.php
@@ -4,6 +4,7 @@ namespace app\components;
 
 use app\models\UserInfo;
 use common\models\User;
+use app\components\exceptions\InvalidDateException;
 
 class UserService
 {
@@ -13,7 +14,6 @@ class UserService
     public $surname;
     public $dateofbirth;
     public $email;
-     
 
     public function __construct($user_id)
     {
@@ -32,51 +32,104 @@ class UserService
     public static function getName($id)
     {
         $user = UserInfo::find()
-                        ->select('user_name')
-                        ->where(['user_id' => $id])
-                        ->one();
-        return isset($data['user_name']) ? $data['user_name'] : null;
+                ->select('user_name')
+                ->where(['user_id' => $id])
+                ->one();
+        return isset($data['user_name']) ? $data['user_name'] : false;
     }
 
     public static function getSurname($id)
     {
         $data = UserInfo::find()
-                        ->select('user_surname')
-                        ->where(['user_id' => $this->id])
-                        ->one();
-        return isset($data['user_surname']) ? $data['user_surname'] : null;
+                ->select('user_surname')
+                ->where(['user_id' => $this->id])
+                ->one();
+        return isset($data['user_surname']) ? $data['user_surname'] : false;
     }
-    
+
     public static function getNameLong($id)
     {
         return "TODO"; // Surname + name
     }
-    
-    public static function getBirthDate($id)
+
+    public static function getBirthDate($id, $option = "default")
     {
-        $data =  UserInfo::find()
+        $data = UserInfo::find()
                 ->select("user_birthdate")
                 ->where(['user_id' => $id])
                 ->one();
-        return isset($data['user_birthdate']) ? $data['user_birthdate'] : null;
+
+        if (!isset($data["user_birthdate"]))
+            return false;
+
+        switch ($option)
+        {
+            case 'object':
+                try
+                {
+                    return self::getDateObj($text);
+                }
+                catch (InvalidDateException $ex)
+                {
+                    //is not date (wtf, someone has edited db manually)
+                    return false;
+                }
+                break;
+            case 'default':
+                return isset($data['user_birthdate']) ? $data['user_birthdate'] : false;
+            default:
+                try
+                {
+                    return self::getMaskedDate($data["user_birthdate"], $option);
+                }
+                catch (InvalidDateException $ex)
+                {
+                    return false;
+                }
+        }
     }
-    
+
     public static function getEmail($id)
     {
         $data = User::find()
                 ->select("email")
                 ->where(['id' => $id])
                 ->one();
-        return isset($data['email']) ? $data['email'] : null;
+        return isset($data['email']) ? $data['email'] : false;
     }
-    
+
     public static function getUserName($id)
     {
-        $data =  User::find()
+        $data = User::find()
                 ->select("username")
                 ->where(['id' => $id])
                 ->one();
-        return isset($data['username']) ? $data['username'] : null;
+        return isset($data['username']) ? $data['username'] : false;
+    }
+
+    private static function getDateObj($text)
+    {
+        if (strtotime($text) !== false)
+        {
+            return \DateTime::createFromFormat('Y-m-d', $text);
+        }
+        else
+        {
+            throw new InvalidDateException("INVALID BIRTH DATE IN DATABASE", 1);
+        }
+    }
+
+    private static function getMaskedDate($date, $mask)
+    {
+        try
+        {
+            $date = self::getDateObj($date);
+        }
+        catch (Exception $ex)
+        {
+            throw new InvalidDateException("INVALID DATE");
+        }
+        return $date->format($mask);
     }
 
 }
