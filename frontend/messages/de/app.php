68bee4d6ade3eb61d31f1756631d14427e4cfc5c
Greg Lieske <grzesiu.lieske@gmail.com>
2015-12-07 00:11:11 +0100

    Lang files fix

--- frontend/messages/de/app.php
+++ frontend/messages/de/app.php
@@ -6,18 +6,18 @@ return [
     'Sign in' => 'Anmelden',
     "You've not signed in" => 'Sie haben keine Artikel im Warenkorb',
     'Register'=> 'Registrieren',
-    'LogIn'=> 'Einloggen'
-    'Main Navigation' =>'Hauptnavigation'
-    'Home' =>'Zuhause'
-    'Sign in to start your session' =>'Melden Sie sich an , um Ihre Sitzung zu starten'
-    'I forgot my password' => 'Ich habe mein Passwort vergessen'
-    'Request password reset' => 'Anfrage zum Zurücksetzen des Passworts'
-    'Please choose your new password'=> 'Bitte geben Sie Ihr neues Passwort'
-    'Username'=>'Benutzername'
-    'Password'=>'Passwort'
-    'Retype Password'=>'Passwort erneut eingeben'
-    'Register a new membership'=>'Registrieren Sie einen neuen Mitgliedschaft'
-    'I already have a membership'=>'Ich habe bereits eine Mitgliedschaft'
+    'LogIn'=> 'Einloggen',
+    'Main Navigation' =>'Hauptnavigation',
+    'Home' =>'Zuhause',
+    'Sign in to start your session' =>'Melden Sie sich an , um Ihre Sitzung zu starten',
+    'I forgot my password' => 'Ich habe mein Passwort vergessen',
+    'Request password reset' => 'Anfrage zum Zurücksetzen des Passworts',
+    'Please choose your new password'=> 'Bitte geben Sie Ihr neues Passwort',
+    'Username'=>'Benutzername',
+    'Password'=>'Passwort',
+    'Retype Password'=>'Passwort erneut eingeben',
+    'Register a new membership'=>'Registrieren Sie einen neuen Mitgliedschaft',
+    'I already have a membership'=>'Ich habe bereits eine Mitgliedschaft',
     
     
 ];
--- frontend/messages/pl/app.php
+++ frontend/messages/pl/app.php
@@ -7,18 +7,18 @@ return [
     'Sign in' => 'Zaloguj',
     "You've not signed in" => 'Nie jestes zalogowany',
     'Register'=> 'Zarejestruj sie',
-    'LogIn'=> 'Zaloguj sie'
-    'Main Navigation' =>'Pasek nawigacji'
-    'Home' =>'Strona glowna'
-    'Sign in to start your session' =>'Zaloguj sie zeby korzystać z serwisu'
-    'I forgot my password' => 'Zapomnialem hasla'
-    'Request password reset' => 'zadanie resetowania hasla'
-    'Please choose your new password'=> 'Prosze wpisac nowe haslo'
-    'Username'=>'Nazwa uzytkownika'
-    'Password'=>'Haslo'
-    'Retype Password'=>'Powtorz haslo'
-    'Register a new membership'=>'Utworz konto'
-    'I already have a membership'=>'Jestem już uzytkownikiem'
+    'LogIn'=> 'Zaloguj sie',
+    'Main Navigation' =>'Pasek nawigacji',
+    'Home' =>'Strona glowna',
+    'Sign in to start your session' =>'Zaloguj sie zeby korzystać z serwisu',
+    'I forgot my password' => 'Zapomnialem hasla',
+    'Request password reset' => 'zadanie resetowania hasla',
+    'Please choose your new password'=> 'Prosze wpisac nowe haslo',
+    'Username'=>'Nazwa uzytkownika',
+    'Password'=>'Haslo',
+    'Retype Password'=>'Powtorz haslo',
+    'Register a new membership'=>'Utworz konto',
+    'I already have a membership'=>'Jestem już uzytkownikiem',
     
     
 ];
