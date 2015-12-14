64a49d535543c4d4d870438fc6e148125d91550d
Przemyslaw Blokus <przemek.blokus@gmail.com>
2015-12-14 16:14:56 +0100

    FollowService and FriendService empty files created.

---
+++ frontend/components/FollowService.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\components;
+
+use app\models\UserInfo;//wtf
+use common\models\User;//wtf
+use app\components\exceptions\InvalidDateException;
+use app\components\exceptions\InvalidUserException;
+
+
+class FollowService
+{
+     public function __construct()
+     {
+         
+     }
+     
+    //TO DO FOR GRZESIEK
+}
---
+++ frontend/components/FriendService.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\components;
+
+use app\models\UserInfo;//wtf
+use common\models\User;//wtf
+use app\components\exceptions\InvalidDateException;
+use app\components\exceptions\InvalidUserException;
+
+
+class FriendService
+{
+     public function __construct()
+     {
+         
+     }
+     
+    //TO DO FOR GRZESIEK
+}
+
+
