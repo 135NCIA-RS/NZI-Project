ca55d6b5e5f4ae7646bd47577bd600f04c92a5c2
Błażej Darowski <darowskib@gmail.com>
2016-01-11 21:55:59 +0100

    dependency injection tutorial

---
+++ frontend/tutorials/dependency injection.php
@@ -0,0 +1,40 @@
+
+
+//LEWA STRONA TABLICY
+
+use yii/di/container;
+
+class (... {
+-> public $pl=null;
+public function _construct($id, $module, ProductsList $pl, $config=[] {
+-> $this -> = $pl;
+parent::__construct($id, $module, $config);
+}
+p.f.action Index(){
+$list=$this->pl/*$pl*/->get(); ------,
+return $this -> new(      ,('list'=>$this.J);
+}  
+            DI-cooping
+
+
+
+//PRAWA STRONA TABLICY
+
+class Products List
+{ pok $is=null;
+public( fu.__construct(InnySerwis $s){
+$this->J=$s;
+}
+
+
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
