15a683cff5bdcae98a73b0b71d0d7da2127a6338
Przemyslaw Blokus <przemek.blokus@gmail.com>
2016-01-13 11:18:23 +0100

    RequestService working copy

---
+++ frontend/models/Request.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "request".
+ *
+ * @property integer $req_id
+ * @property integer $user1_id
+ * @property integer $user2_id
+ * @property string $req_type
+ * @property string $date
+ */
+class Request extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'request';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['req_id', 'user1_id', 'user2_id', 'req_type', 'date'], 'required'],
+            [['req_id', 'user1_id', 'user2_id'], 'integer'],
+            [['date'], 'safe'],
+            [['req_type'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'req_id' => 'Req ID',
+            'user1_id' => 'User1 ID',
+            'user2_id' => 'User2 ID',
+            'req_type' => 'Req Type',
+            'date' => 'Date',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return RequestQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new RequestQuery(get_called_class());
+    }
+}
