0940a7be82cd249f4e775ebccc2da3792a9596ae
Błażej Darowski <darowskib@gmail.com>
2016-01-21 17:14:38 +0100

    view allRequests and new add post view

--- frontend/views/intouch/allRequests.php
+++ frontend/views/intouch/allRequests.php
@@ -10,25 +10,23 @@ $count = $this->params['notification_count'];
 foreach ($data as $var)
 {
     ?>
-    <div>
+    <div style="background-color: #dcebef; padding: 10px 10px 10px 10px; border-radius: 10px;  margin-bottom:5px;">
         <div>
+            <?= Html::beginForm("", 'post', []) ?>
             <?php
             if ($var['type'] == RequestType::FriendRequest)
             {
                 ?>
 
                 <?= $var['date'] ?> | <?= $var['senderUserName'] ?> sent you an friendship request.
-
                 <?php
             }
             ?>
-        </div>
-        <div>
-            <?= Html::beginForm("", 'post', []) ?>
-            <button type="submit" name="accept-btn" style=" width: 100px;" class="btn btn-danger pull-right btn-block btn-sm">Accept</button>
-            <button type="submit" name="dismiss-btn" style=" width: 100px;" class="btn btn-danger pull-right btn-block btn-sm">Dismiss</button>
+            <button type="submit" name="dismiss-btn" style=" width: 100px; margin-left: 5px; margin-top: -5px;" class="btn btn-danger pull-right btn-sm">Dismiss</button>
+            <button type="submit" name="accept-btn" style=" width: 100px; margin-top: -5px;" class="btn btn-success pull-right btn-sm">Accept</button>
             <input type="hidden" name="request_id" value="<?= $var['req_id'] ?>">
             <?= Html::endForm() ?>
+            
         </div>
     </div>
     <?php
--- frontend/views/intouch/profile.php
+++ frontend/views/intouch/profile.php
@@ -83,6 +83,24 @@ use app\components\PhotoService;
                 <div class="tab-content">
                     <div class="active tab-pane" id="activity">
                         <!-- Add post -->
+                        <div class="tab-content">
+                    <div class="active tab-pane" id="activity">
+                        <!-- Add post -->
+
+                        <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Default Modal</h4>
+              </div>
+              <div class="modal-body">
+                <p>One fine body…</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
                         <?= Html::beginForm("", 'post', []) ?>
                         <input class="form-control input-sm send-form-input" row="3" type="text" placeholder="Post" name="inputText">
                         <input type="hidden" name="type" value="newpost">
