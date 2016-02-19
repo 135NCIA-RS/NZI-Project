bdee5929c9bbb3f24d14875dfc00628abffc14fa
Greg Lieske <grzesiu.lieske@gmail.com>
2016-02-19 20:54:46 +0100

    This resolves problem with applying migrations

--- console/migrations/m160219_184316_RBAC_Tables.php
+++ console/migrations/m160219_184316_RBAC_Tables.php
@@ -9,15 +9,14 @@ class m160219_184316_RBAC_Tables extends Migration
     public function up()
     {
         $sql = <<< KONIEC
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";		
+
 CREATE TABLE `auth_rule` (
  `name` varchar(64) NOT NULL,
  `data` text,
  `created_at` int(11) DEFAULT NULL,
  `updated_at` int(11) DEFAULT NULL,
  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `auth_item` (
  `name` varchar(64) NOT NULL,
@@ -31,7 +30,7 @@ CREATE TABLE `auth_item` (
  KEY `rule_name` (`rule_name`),
  KEY `type` (`type`),
  CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `auth_item_child` (
  `parent` varchar(64) NOT NULL,
@@ -40,7 +39,7 @@ CREATE TABLE `auth_item_child` (
  KEY `child` (`child`),
  CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
  CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `auth_assignment` (
  `item_name` varchar(64) NOT NULL,
@@ -50,7 +49,7 @@ CREATE TABLE `auth_assignment` (
  KEY `user_id` (`user_id`),
  CONSTRAINT `auth_assignment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
  CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 REPLACE INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
 ('admin', 1, 'Administrative rights', NULL, NULL, NULL, NULL),
