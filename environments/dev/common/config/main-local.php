4d020d72d3f0ce58515da0db72cb9de381296b0a
Greg Lieske <grzesiu.lieske@gmail.com>
2016-02-29 20:54:23 +0100

    Mail fix 2

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -8,14 +8,6 @@ return [
             'password' => 'root',
             'charset' => 'utf8',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
 //        'gii' => [
 //            'class' => 'yii\gii\Module',
 //            'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'],
