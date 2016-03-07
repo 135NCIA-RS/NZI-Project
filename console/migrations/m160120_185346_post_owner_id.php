31faab7dc2ba62663375a61e7f494c0111a8e70e
Greg Lieske <grzesiu.lieske@gmail.com>
2016-03-07 20:50:30 +0100

    Next Migration FIX

--- console/migrations/m160120_185346_post_owner_id.php
+++ console/migrations/m160120_185346_post_owner_id.php
@@ -13,7 +13,7 @@ class m160120_185346_post_owner_id extends Migration
         $this->execute($sql);
         $sql = "ALTER TABLE `post` ADD INDEX(`owner_id`);";
         $this->execute($sql);
-        $sql = "ALTER TABLE `post` ADD FOREIGN KEY (`owner_id`) REFERENCES `projectdb`.`user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;\n"
+        $sql = "ALTER TABLE `post` ADD FOREIGN KEY (`owner_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;\n"
     . "";
         $this->execute($sql);
         $sql = "SET foreign_key_checks = 1;";
